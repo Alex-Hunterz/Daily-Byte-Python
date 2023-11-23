276824138fe7f042f88f45e45d2d0cf7b34cd27d
Manit Bohra <manit.bohra@outlook.com>
2023-11-23 08:09:21 +0530

    2nd Commit

--- main.py
+++ main.py
@@ -6,15 +6,23 @@
 # “The Daily Byte”, return "etyB yliaD ehT”
 # “civic”, return “civic”
 
-def is_rev(s):
-    """Function returning Reverse"""
-    return s[::-1]
-inp = input('Enter the str: ')
+# def is_rev(s):
+#     """Function returning Reverse"""
+#     return s[::-1]
+# inp = input('Enter the str: ')
 
-print(is_rev(inp))
+# print(is_rev(inp))
 
 # 15th
 # Q2)Given the following strings...
 # "level", return true
 # "algorithm", return false
 # "A man, a plan, a canal: Panama.", return true
+st = input('Enter to check Palindrome:').lower()
+st_clean = "".join(letter for letter in st if letter.isalnum())
+st_rev = st_clean[::-1]
+if st_clean == st_rev:
+    print(True)
+else:
+    print(False)
+
